ae7751a33383f952954b43350d674f223f16ed4f
danestves <danestves@users.noreply.github.com>
2025-05-02 23:14:03 -0400

    refactor: remove SignIn component from login route

--- apps/web/src/routes/_auth/login.$.tsx
+++ apps/web/src/routes/_auth/login.$.tsx
@@ -1,11 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { SignIn } from "@clerk/tanstack-react-start";
-
 export const Route = createFileRoute("/_auth/login/$")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <SignIn />;
+	return null;
 }
