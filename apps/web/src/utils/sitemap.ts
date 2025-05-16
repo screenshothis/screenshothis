98cc10f5be6abc167562a3a7070f825bd567e094
danestves <danestves@users.noreply.github.com>
2025-05-16 13:26:45 -0400

    feat(sitemap): add forgot-password route to sitemap and update legal pages mapping

--- apps/web/src/utils/sitemap.ts
+++ apps/web/src/utils/sitemap.ts
@@ -1,7 +1,6 @@
 import type { Sitemap } from "tanstack-router-sitemap";
 
 import type { FileRouteTypes } from "#/routeTree.gen.ts";
-import { allLegalPages } from "../../.content-collections/generated";
 
 // This will become a string literal union of all your routes
 export type TRoutes = FileRouteTypes["fullPaths"];
@@ -23,9 +22,13 @@ export const sitemap: Sitemap<TRoutes> = {
 			priority: 0.8,
 			changeFrequency: "daily",
 		},
+		"/forgot-password": {
+			priority: 0.8,
+			changeFrequency: "daily",
+		},
 		"/legal/$": async () => {
-			return allLegalPages.map((post) => ({
-				path: `/legal/${post.slug}`,
+			return ["privacy", "terms"].map((page) => ({
+				path: `/legal/${page}`,
 				priority: 0.8,
 				changeFrequency: "daily",
 			}));
