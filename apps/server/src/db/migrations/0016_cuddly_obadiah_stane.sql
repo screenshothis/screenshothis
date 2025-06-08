4d6c9020c16218d50054a82255e385e8ab36ad79
danestves <danestves@users.noreply.github.com>
2025-06-08 14:39:50 -0400

    fix: modify SQL index creation to use CONCURRENTLY and add IF NOT EXISTS for better performance

--- apps/server/src/db/migrations/0016_cuddly_obadiah_stane.sql
+++ apps/server/src/db/migrations/0016_cuddly_obadiah_stane.sql
@@ -1,2 +1,2 @@
 DROP INDEX CONCURRENTLY IF EXISTS "screenshots_workspace_id_idx";--> statement-breakpoint
-CREATE INDEX "screenshots_workspace_cached_idx" ON "screenshots" USING btree ("workspace_id","is_cached");
+CREATE INDEX CONCURRENTLY IF NOT EXISTS "screenshots_workspace_cached_idx" ON "screenshots" USING btree ("workspace_id","is_cached");
