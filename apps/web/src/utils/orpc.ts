a1559683fb6bc87d53b5ef36507486559a5bd1e8
danestves <danestves@users.noreply.github.com>
2025-06-20 14:05:21 -0400

    refactor: simplify RPC link configuration in orpc.ts

--- apps/web/src/utils/orpc.ts
+++ apps/web/src/utils/orpc.ts
@@ -3,33 +3,18 @@ import { RPCLink } from "@orpc/client/fetch";
 import type { RouterClient } from "@orpc/server";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import type { appRouter } from "@screenshothis/server/routers";
-import { createServerFn } from "@tanstack/react-start";
-import { getHeaders } from "@tanstack/react-start/server";
 
 import { env } from "./env.ts";
 
-export const getServerHeaders = createServerFn({ method: "GET" }).handler(
-	() => {
-		return getHeaders();
-	},
-);
-
 export const link = new RPCLink({
-	url: `${env.VITE_SERVER_URL}/rpc`,
-	fetch(url, options) {
-		return fetch(url, {
-			...options,
-			credentials: "include",
-		});
-	},
-	headers: () => {
-		if (typeof window === "undefined") {
-			return getServerHeaders();
-		}
-
-		return {};
-	},
-});
+    url: `${env.VITE_SERVER_URL}/rpc`,
+    fetch(url, options) {
+      return fetch(url, {
+        ...options,
+        credentials: "include",
+      });
+    },
+  });
 
 export const client: RouterClient<typeof appRouter> = createORPCClient(link);
 
